aca00be5777b1679bb47937bde8b6c6094a6430a
Michael McIntyre <michael@tekanics.com>
2013-05-29 11:59:44 -0400

    fix script

--- recipes/formyula.sh
+++ recipes/formyula.sh
@@ -3,12 +3,16 @@ log_file=$1
 # Install Java 7
 echo -e "\n=> Installing Formyula Configuration File..."
 
+P1 = "CREATE DATABASE IF NOT EXISTS 'formyula-enterprise';"
+
+sudo mysql -uroot -p -e "CREATE DATABASE IF NOT EXISTS 'formyula-enterprise';GRANT ALL ON *.* TO 'formyula-db-user'@'localhost' IDENTIFIED BY '1formyula!2';FLUSH PRIVILEGES;"
+
 sudo touch /etc/formyula.yml && sudo sh -c "cat > /etc/formyula.yml" << EOF
 production:
   adapter: jdbcmysql
   encoding: utf8
-  database: your-database-name
-  username: your-database-username
-  password: your-database-password
+  database: formyula-enterprise
+  username: formyula-db-user
+  password: 1formyula!2
 EOF
 
